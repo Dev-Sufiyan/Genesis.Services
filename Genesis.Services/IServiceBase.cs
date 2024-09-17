1edf28435f11eb8051a3edf816426e32fe39d1db
Mohmad Sufiyan <sufiyan.m@surya-soft.com>
2024-09-17 11:53:44 +0530

    ➕ Added Code For Service Base

---
+++ Genesis.Services/IServiceBase.cs
@@ -0,0 +1,16 @@
+﻿using Genesis.Models.DTO;
+
+namespace Genesis.Services
+{
+    public interface IServiceBase<T> where T : class
+    {
+        Task<IEnumerable<T>> GetRecordsAsync(List<SearchParam> searchParams);
+        Task<T> GetByPKAsync(object keyValue);
+        Task<bool> IsRecordPresentAsync(T entity);
+        Task AddAsync(T entity);
+        Task SaveRecordsAsync(IEnumerable<T> records);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task DeleteByPKAsync(object keyValue);
+    }
+}
---
+++ Genesis.Services/ServiceBase.cs
@@ -0,0 +1,55 @@
+﻿using Genesis.Repositories;
+using Genesis.Models.DTO;
+
+namespace Genesis.Services
+{
+    public class ServiceBase<T> : IServiceBase<T> where T : class
+    {
+        protected readonly IRepositoriesBase<T> _repository;
+
+        public ServiceBase(IRepositoriesBase<T> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<T>> GetRecordsAsync(List<SearchParam> searchParams)
+        {
+            return await _repository.GetRecordsAsync(searchParams);
+        }
+
+        public async Task<T> GetByPKAsync(object keyValue)
+        {
+            return await _repository.GetByPKAsync(keyValue);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _repository.AddAsync(entity);
+        }
+
+        public async Task SaveRecordsAsync(IEnumerable<T> records)
+        {
+            await _repository.SaveRecordsAsync(records);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            await _repository.UpdateAsync(entity);
+        }
+
+        public async Task DeleteByPKAsync(object keyValue)
+        {
+            await _repository.DeleteByPKAsync(keyValue);
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            await _repository.DeleteAsync(entity);
+        }
+
+        public async Task<bool> IsRecordPresentAsync(T entity)
+        {
+            return await _repository.IsEntityExistAsync(entity);
+        }
+    }
+}
